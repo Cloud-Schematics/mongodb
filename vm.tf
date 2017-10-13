bf5c2811935a78c30c293b776259fcd3e943b434
Kate Lyons <knlyons@us.ibm.com>
2017-10-13 15:32:41 -0500

    Variable descriptions for the UI

--- vm.tf
+++ vm.tf
@@ -14,72 +14,86 @@
 ################################################################
 variable "hostname" {
   default = "hostname"
+  description = "Hostname for the computing instance."
 }
 variable "domain" {
   default = "domain.dev"
+  description = "Domain for the computing instance."
 }
 variable "datacenter" {
   default = "wdc01"
+  description = "Which data center the VM is to be provisioned in. You can run bluemix cs locations to see a list of all data centers in your region."
 }
 variable "os_reference_code" {
   default = "CENTOS_7"
+  description = "An operating system reference code that is used to provision the computing instance."
 }
 variable "cores" {
   default = "1"
+  description = "The number of CPU cores to allocate."
 }
 variable "memory" {
   default = "1024"
+  description = "The amount of memory to allocate, expressed in MBs."
 }
 variable "disk_size" {
   default = "25"
+  description = "Numeric disk sizes in GBs."
 }
 variable "private_network_only" {
   default = "false"
+  description = "When set to true, a compute instance only has access to the private network."
 }
 variable "network_speed" {
   default = "100"
+  description = "The connection speed (in Mbps) for the instance’s network components."
 }
 variable "tags" {
   default = ""
+  description = "Set tags on the VM instance."
 }
 
 variable "ssh_user" {
   default = "root"
-  description = "default user for VM"
+  description = "The default user for the VM."
 }
 variable "ssh_label" {
   default = "public ssh key - Schematics VM"
+  description = "An identifying label to assign to the SSH key."
 }
 variable "ssh_notes" {
   default = ""
+  description = "Notes to store with the SSH key."
 }
 variable "public_key" {
   default = ""
-  description = "public SSH key to use in keypair"
+  description = "Your public SSH key to use for access to the VM."
 }
 
 variable temp_public_key {
     default = ""
+    description = "The temporary private key for the SSH key pair to use for provisioning."
 }
 variable temp_private_key {
     default = ""
+    description = "The temporary public key of the SSH key pair to use for provisioning."
 }
 
 variable "install_script" {
   default = "files/installMongoDB.sh"
-  description = "installation script path"
+  description = "The relative location of the Mongo DB install script."
 }
 variable "script_variables" {
   default = ""
-  description = "variables to pass into script"
+  description = "The variables to pass into the script."
 }
 variable "sample_application_url" {
   default = ""
-  description = "sample application URL"
+  description = "The sample application URL."
 }
 variable "custom_commands" {
   default = "sleep 1"
-  description = "custom commands to run"
+  description = "Custom commands to run."
 }
 
 resource "ibm_compute_ssh_key" "ssh_key" {
