758331c21bf3788a48b26ba8557a1f2e409d759b
vaishnavi <ggunishetty9@gmail.com>
2019-09-11 11:21:30 +0530

    Fixed the description of temp private and public key

--- vm.tf
+++ vm.tf
@@ -13,99 +13,102 @@
 #
 ################################################################
 variable "hostname" {
-  default = "hostname"
+  default     = "hostname"
   description = "Hostname for the computing instance."
 }
+
 variable "domain" {
-  default = "domain.dev"
+  default     = "domain.dev"
   description = "Domain for the computing instance."
 }
+
 variable "datacenter" {
-  default = "wdc01"
-  description = "Which data center the VM is to be provisioned in. You can run bluemix cs locations to see a list of all data centers in your region."
+  default     = "wdc01"
+  description = "Which data center the VM is to be provisioned in. You can run ic cs zones to see a list of all data centers in your region."
 }
+
 variable "os_reference_code" {
-  default = "CENTOS_7"
+  default     = "CENTOS_7"
   description = "An operating system reference code that is used to provision the computing instance."
 }
+
 variable "cores" {
-  default = "1"
+  default     = "1"
   description = "The number of CPU cores to allocate."
 }
+
 variable "memory" {
-  default = "1024"
+  default     = "1024"
   description = "The amount of memory to allocate, expressed in MBs."
 }
+
 variable "disk_size" {
-  default = "25"
+  default     = "25"
   description = "Numeric disk sizes in GBs."
 }
+
 variable "private_network_only" {
-  default = "false"
+  default     = "false"
   description = "When set to true, a compute instance only has access to the private network."
 }
+
 variable "network_speed" {
-  default = "100"
+  default     = "100"
   description = "The connection speed (in Mbps) for the instance’s network components."
 }
+
 variable "tags" {
-  default = ""
+  default     = ""
   description = "Set tags on the VM instance."
 }
 
 variable "ssh_user" {
-  default = "root"
+  default     = "root"
   description = "The default user for the VM."
 }
+
 variable "ssh_label" {
-  default = "public ssh key - Schematics VM"
+  default     = "public ssh key - Schematics VM"
   description = "An identifying label to assign to the SSH key."
 }
+
 variable "ssh_notes" {
-  default = ""
+  default     = ""
   description = "Notes to store with the SSH key."
 }
+
 variable "public_key" {
-  default = ""
   description = "Your public SSH key to use for access to the VM."
 }
 
-variable temp_public_key {
-    default = ""
-    description = "The temporary private key for the SSH key pair to use for provisioning."
-}
-variable temp_private_key {
-    default = ""
-    description = "The temporary public key of the SSH key pair to use for provisioning."
+variable "private_key" {
+  description = "The private SSH key to use for access to the VM."
 }
 
 variable "install_script" {
-  default = "files/installMongoDB.sh"
+  default     = "files/installMongoDB.sh"
   description = "The relative location of the Mongo DB install script."
 }
+
 variable "script_variables" {
-  default = ""
+  default     = ""
   description = "The variables to pass into the script."
 }
+
 variable "sample_application_url" {
-  default = ""
+  default     = ""
   description = "The sample application URL."
 }
+
 variable "custom_commands" {
-  default = "sleep 1"
+  default     = "sleep 1"
   description = "Custom commands to run."
 }
 
 resource "ibm_compute_ssh_key" "ssh_key" {
-    label = "${var.ssh_label}"
-    notes = "${var.ssh_notes}"
-    public_key = "${var.public_key}"
-}
-
-resource "ibm_compute_ssh_key" "temp_ssh_key" {
-    label = "mytempkey"
-    notes = "mynotes"
-    public_key =  "${var.temp_public_key}"
+  label      = "${var.ssh_label}"
+  notes      = "${var.ssh_notes}"
+  public_key = "${var.public_key}"
 }
 
 resource "ibm_compute_vm_instance" "vm" {
@@ -121,13 +124,14 @@ resource "ibm_compute_vm_instance" "vm" {
   disks                    = ["${var.disk_size}"]
   dedicated_acct_host_only = true
   local_disk               = false
-  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}", "${ibm_compute_ssh_key.temp_ssh_key.id}"]
+  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
   tags                     = ["${var.tags}"]
+
   connection {
     user        = "${var.ssh_user}"
-    private_key = "${var.temp_private_key}"
+    private_key = "${var.private_key}"
   }
-  
+
   # Create the installation script
   provisioner "file" {
     source      = "${var.install_script}"
@@ -139,11 +143,11 @@ resource "ibm_compute_vm_instance" "vm" {
     inline = [
       "chmod +x installation.sh",
       "bash installation.sh ${var.sample_application_url} ${var.script_variables}",
-      "${var.custom_commands}"
+      "${var.custom_commands}",
     ]
   }
 }
 
 output "public_ip" {
-	value = "http://${ibm_compute_vm_instance.vm.ipv4_address}"
+  value = "http://${ibm_compute_vm_instance.vm.ipv4_address}"
 }
